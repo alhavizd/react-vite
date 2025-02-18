6c60f011f2e0200754437fa7498316cb88b5924e
Luthfi Alhavizd <alhavizd.luthfi@gmail.com>
2025-02-18 09:49:36 +0700

    remove middleware

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,10 +1,12 @@
 import React, {createContext, useContext, useState, useEffect} from 'react'
+import axios from 'axios'
+import i18n from 'i18next'
 import {useQuery} from '@tanstack/react-query'
 import {addSeconds} from 'date-fns'
 import {login} from '@/services/auth'
 import {setToken} from '@/utils/token'
+import {getDomain, setDomain} from '@/utils/domain'
 import {GlobalContextType} from '@/types/global'
-import {checkStore} from '@/middleware/checkStore' // Import checkStore
 
 const GlobalContext = createContext<GlobalContextType | undefined>(undefined)
 
@@ -25,12 +27,43 @@ export const GlobalProvider = ({children}: {children: React.ReactNode}) => {
     document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light')
   }, [isDarkMode])
 
+  const colors = isDarkMode ? {primary: '#0d6efd', secondary: '#adb5bd', text: '#f8f9fa'} : {primary: '#007bff', secondary: '#6c757d', text: '#000'}
+
   // Menjalankan checkStore saat pertama kali aplikasi dimuat
   useEffect(() => {
-    checkStore() // Tambahkan ini
-  }, [])
+    const checkStore = async () => {
+      if (!getDomain()) {
+        const isDevelopment = import.meta.env.VITE_NODE_ENV === 'development'
+        const host = window.location.hostname
+        let url_store = 'jakartatest'
 
-  const colors = isDarkMode ? {primary: '#0d6efd', secondary: '#adb5bd', text: '#f8f9fa'} : {primary: '#007bff', secondary: '#6c757d', text: '#000'}
+        if (isDevelopment) {
+          url_store = 'jakartatest'
+          return setDomain(url_store)
+        }
+        if (host.includes('antarinmakan.com') || host.includes('indociti.com')) {
+          url_store = host.includes('indociti.com') ? 'jakartatest' : host.split('.')[0] || 'jakartatest'
+          return setDomain(url_store)
+        }
+        if (!host.includes('antarinmakan.com') && !host.includes('indociti.com')) {
+          try {
+            const hostname = window.location.hostname
+            const selectedLocale = i18n.language || import.meta.env.VITE_I18N_LOCALE
+            const {data} = await axios.post(`${import.meta.env.VITE_API_URL}${selectedLocale}/hintstore`, {domain: hostname})
+            url_store = data.data.url_id
+            return setDomain(url_store)
+          } catch {
+            url_store = 'jakartatest'
+            return setDomain(url_store)
+          }
+        }
+        url_store = host.split('.').length > 2 ? host.split('.')[0] : 'jakartatest'
+        return setDomain(url_store)
+      }
+    }
+
+    checkStore() // panggil langsung di sini
+  }, [])
 
   const {
     data: token,
